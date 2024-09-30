e2acc9b44eca65ac51f2f61b17052269dc459c1d
MoSheckon <mch239@nau.edu>
2024-09-30 12:01:17 -0700

    This is a good starting point.

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+const app = express(); 
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
