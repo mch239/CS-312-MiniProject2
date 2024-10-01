84d3976232f3d812001466ffbaa89575ca749a4e
MoSheckon <mch239@nau.edu>
2024-09-30 18:49:43 -0700

    Luckily there are available resources. Break 4.

--- index.js
+++ index.js
@@ -1,80 +1,96 @@
-import express from "express";
+import express from 'express';
+import axios from 'axios';
 import bodyParser from "body-parser";
-import axios from "axios";
 
-const app = express(); 
+const app = express();
 const port = 3000;
 
-//I put in the Programming category avoid any offensive jokes that may appear.
-const API_URL = 'https://v2.jokeapi.dev/joke/Programming';
+//THIS URL LEADS TO THE 'SAFE' JOKES WITHIN THE JOKE API
+const API_URL = 'https://v2.jokeapi.dev/joke/Any?safe-mode'; 
 
-//I have to code in the neccessary functions to convert the name parameter into a joke. 
-
+//THIS FUNCTIONS CONVERTS THE NAME INPUTTED INTO AN ID
 function userNameAsNumber(name) {
-  name = name.toLowerCase()
-  let result = 0;
-
-  for (let i = 0; i < name.length; i++ ){
-      result += name.charCodeAt(i);
-  }
-}
-return result % 345;
-//this puts in place a boundary on id length aka the range of jokes
-
-
-//Function to find only the safe jokes to share, there are some rather inappropriate ones in the mix. 
-function findSafeJoke(jokes) {
-  let result = null; 
-  for (const joke of jokes) {
-    if(joke.safe === true)  {
-        result = joke;
+    name = name.toLowerCase()
+    let result = 0;
+    
+    for (let i = 0; i < name.length; i++) {
+        result += name.charCodeAt(i);
     }
-  }
-  if (!result) {
-      return jokes[0];
-  }
-  return result;
+    return result % 450; 
+    
 }
 
-//Formatting the layout of the Jokes so it does not look exactly like the output on the site
+//FINDING A JOKE THAT IS TRULY SAFE
+function findJoke(jokes) {
+    let result = null;
+    for (const joke of jokes) {
+        if (joke.safe === true) {
+            result = joke;
+        }
+    }
+    if (!result) {
+        return jokes[0];
+    }
+    return result;
+}
+
+
+//FORMATTING THE OUTPUT OF THE JOKE, IN CASE THE CODING DOESN'T READ THE JOKES' CODE FORMATTING
 function formatJoke(input) {
-  if (input.joke && input.joke.includes('\n')) {
-    input.joke = input.joke.split('\n').join('<br>');
-  }
+    if (input.joke && input.joke.includes('\n')) {
+        input.joke = input.joke.split('\n').join('<br>');
+    }
 
-  if (input.setup){
-    if (input.setup.includes('\n')) {
-      input.setup = input.setup.split('\n').join('<br>');
+    if (input.setup) {
+        if (input.setup.includes('\n')) {
+            input.setup = input.setup.split('\n').join('<br>');
+        }
+        else if (input.delivery.includes('\n')) {
+            input.delivery = input.delivery.split('\n').join('<br>');
+        }
     }
-    else if (input.delivery.includes('\n')) {
-      input.delivery = input.delivery.split('\n').join('<br>');
-    }
-  }
-  return input;
+    return input;
 }
 
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true}));
+//MIDDLEWARES
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-//This is where I link the API url to my homepage
 
 app.get("/", async (req, res) => {
-  try {
-    const response = await axios.get("");
-    const result = response.data;
-    res.render("index.ejs", { data: result });
-  } catch (error) {
-    console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
-      error: error.message,
-    });
-  }
+    try {
+      const response = await axios.get(API_URL);
+      const result = response.data;
+      res.render("index.ejs", { data: result });
+    } catch (error) {
+      console.error("Failed to make request:", error.message);
+      res.render("index.ejs", {
+        error: error.message,
+      });
+    }
+  });
+
+app.post('/submit', async (req, res) => {
+    try {
+        const fullName = `${req.body.firstName}${req.body.lastName}`;
+        const firstID = userNameAsNumber(fullName); 
+        const lastID = firstID + 9; 
+    
+        const result = await axios.get(API_URL + firstID + '-' + lastID + '&amount=10');
+        const chosenJoke = findJoke(result.data.jokes); 
+        const formattedJoke = formatJoke(chosenJoke); 
+
+        res.render('index.ejs', { 
+            joke: formattedJoke 
+        });
+
+        res.sendStatus(200);
+    } catch (error) {
+        console.log(error.message)
+        res.status(500);
+    }
 });
-
-
-
-
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-  });
+});
